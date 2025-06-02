b59dd8fdf12dd157fb2ca63df6efeee8af6ae563
Benjamin FitzGerald <bfitzg18@yahoo.com>
2025-06-02 14:58:13 -0500

    Update 1p.tarWAPs.sh

--- scripts/1. PP2WAP/1p.tarWAPs.sh
+++ scripts/1. PP2WAP/1p.tarWAPs.sh
@@ -1,11 +1,10 @@
+# This executable moves the WAP output files in to a subfolder and tars them based on year which is how the should be given to the AMC job submission
 mkdir -p $1/WAPs
 
 cd $1
-#mv -p WAP.*.nc4 WAPs
+mv -p WAP.*.nc4 WAPs
 
 cd WAPs
-
-rm WAPs.*.tar.gz
 
 for i in $(seq $2 $3); do
   tar cfz WAPs.$i.$4.tar.gz WAP.$i*.$4.nc4
