f3abfe81711693add800e9c18f4145065bc411a2
Benjamin FitzGerald <bfitzg18@yahoo.com>
2025-06-02 15:01:02 -0500

    Update 1p.tarWAPs.sh

--- scripts/1. PP2WAP/1p.tarWAPs.sh
+++ scripts/1. PP2WAP/1p.tarWAPs.sh
@@ -1,5 +1,10 @@
-# This executable moves the WAP output files in to a subfolder and tars them based on year which is how the should be given to the AMC job submission
-mkdir -p $1/WAPs
+# ============================================
+# Script Name:    1p.tarWAPs.sh
+# Author:         Benjamin FitzGerald
+# Date:           2025-06-02
+# Description:    This executable moves the WAP output files in to a subfolder and tars them based on year which is how the should be given to the AMC job submissions
+# Usage:          ./1p.tarWAPs.sh <job directory> <year1> <year2> <File Key>
+# ============================================
 
 cd $1
 mv -p WAP.*.nc4 WAPs
